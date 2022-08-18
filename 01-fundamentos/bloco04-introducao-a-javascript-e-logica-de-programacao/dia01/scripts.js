20d868d15f3fdbf97d639fdb0cf04d5db66e2bb7
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-08-18 13:56:53 -0300

    Conclui quesito 01

---
+++ 01-fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia01/scripts.js
@@ -0,0 +1,26 @@
+const a = 5;
+const b = 3;
+const c = 7;
+let operacao = null;
+
+operacao = "+";
+
+switch (operacao) {
+  case "+":
+    console.log(a+b);
+    break;
+    case "-":
+    console.log(a-b);
+    break;
+    case "*":
+    console.log(a*b);
+    break;
+    case "/":
+    console.log(a/b);
+    break;
+    case "%":
+    console.log(a%b);
+    break;
+    default:
+      console.log("Operação Inválida")
+}
