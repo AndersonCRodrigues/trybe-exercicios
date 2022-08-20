e27d0bd52338748bc4f5b474b6e8abad2375b9bc
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-08-20 12:20:10 -0300

    Conclui bonus 01

---
+++ 01-fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia03/Bonus01.js
@@ -0,0 +1,12 @@
+//Desenha quadrilátero com asteríscos
+let n = 5;
+let star = "*";
+let temp = "";
+
+for (let i = 0; i < n; i += 1){
+  temp += star;
+}
+
+for (let i =0; i < n; i += 1) {
+  console.log(temp);
+}
