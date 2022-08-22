2fa3a700511df020a4a5b0ed9bfb7d31cc6e786e
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-08-22 15:53:51 -0300

    Conclui bonus programa 03

---
+++ 01-fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia04/ProgramaBonus03.js
@@ -0,0 +1,43 @@
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+function criaObjeto (vetor) {
+  let cesta = {};
+  for (let i = 0; i < vetor.length; i += 1){
+    let key = vetor[i];
+    if (!cesta[key]) {
+      cesta[key] = 0;
+    }
+    cesta[key] += 1;
+  }
+  imprime(cesta);
+}
+
+function imprime (cesta) {
+  let summaries = [];
+  for (let frutas in cesta) {
+    let message = `${cesta[frutas]} ${frutas}`;
+    if (cesta[frutas] > 1) message += 's';
+    summaries.push(message);
+  }
+  console.log(`Sua cesta possui: ${summaries.join(', ')}.`);
+}
+
+criaObjeto(basket);
