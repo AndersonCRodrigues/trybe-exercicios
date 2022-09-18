b6dbb3ac52fdc8b1da5f04f24107f44ba7d8eea5
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-09-18 10:41:44 -0300

    adiciona exercicio bonus

--- 01-fundamentos/bloco07-JS-ES6-Teste-Unitário/dia02/exercicio03/parte01.js
+++ 01-fundamentos/bloco07-JS-ES6-Teste-Unitário/dia02/exercicio03/parte01.js
@@ -58,5 +58,57 @@ const obtemValor = (obj, index) => {
 
 const verifica = (obj, key, value) => obj[key] === value ? true : false;
 
-console.log(verifica(lesson3, 'turno', 'noite'));
-console.log(verifica(lesson3, 'materia', 'Maria Clara'));
+// console.log(verifica(lesson3, 'turno', 'noite'));
+// console.log(verifica(lesson3, 'materia', 'Maria Clara'));
+
+const estudantesMatematica = (obj) => {
+  let total = 0;
+  const array = Object.keys(obj);
+  console.log(array);
+  for (index in array) {
+    if(obj[array[index]].materia === 'Matemática'){
+    total += obj[array[index]].numeroEstudantes;
+    }
+  }
+  console.log(total);
+}
+
+// estudantesMatematica(allLessons);
+
+function testaParametros(obj, nome) {
+  const lessons = Object.keys(obj);
+  let bool;
+  for (i of lessons) {
+    if (obj[i].professor === nome) bool = true;
+  }
+  if (!bool) throw new Error ('Nome do(a) professor(a) não encontrado');
+}
+
+const info = (obj, nome) => {
+  const lessons = Object.keys(obj);
+  let nomeMateria = [];
+  let numeroAlunos = 0;
+  for (i of lessons) {
+    if (obj[i].professor === nome) {
+      nomeMateria.push(obj[i].materia);
+      numeroAlunos += obj[i].numeroEstudantes;
+    }
+  }
+  return {materia: nomeMateria.join(', '), estudantes: numeroAlunos};
+};
+
+const relatorio = (obj, nome) => {
+  try {
+    testaParametros(obj, nome);
+    const relato = {professor: nome};
+    Object.assign(relato, info(obj, nome));
+    console.log(relato);
+  }
+  catch (erro) {
+    console.log(erro.message);
+  }
+};
+
+
+
+relatorio(allLessons, 'Maria Clara');
