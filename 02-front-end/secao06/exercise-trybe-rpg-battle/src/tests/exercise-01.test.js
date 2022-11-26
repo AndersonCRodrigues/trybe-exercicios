4f8c86ae79dc228670b7b353cd8664df2b528ee5
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-11-26 08:55:58 -0300

    atualiza front e cria back

---
+++ 02-front-end/secao06/exercise-trybe-rpg-battle/src/tests/exercise-01.test.js
@@ -0,0 +1,12 @@
+import rollDice from '../service/rollDice';
+
+it('testa a função `rollDice`', () => {
+  /*
+    Exercício 01
+  */
+  jest.spyOn(global.Math, 'random').mockReturnValue(0.8);
+
+  expect(rollDice(20)).toBe(16);
+});
+
+
